35656e8db736510f28b8629fa36c4421fdec4bd2
Phuc <tranquangphuc98@gmail.com>
2020-05-25 13:44:35 +0700

    update

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/new-angular-dashboard'));
-
-app.get('*', (req, res) => {
-    res.sendFile(`./front-end/new-angular-dashboard/index.html`); // load the single view file (angular will handle the page changes on the front-end)
-});
-
-// Start the app by listening on the default Heroku port
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/new-angular-dashboard/index.html'));});
 app.listen(process.env.PORT || 8080);
